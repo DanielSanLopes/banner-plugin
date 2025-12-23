b5e929dbc4b894055bf208b3bb82ef31b468ea74
DanielSanLopes <34308537+DanielSanLopes@users.noreply.github.com>
2025-12-22 22:37:49 -0300

    returning \`text/HTML\`

--- app/api/banners/route.ts
+++ app/api/banners/route.ts
@@ -1,12 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function getting(request: NextRequest) {
   const { searchParams } = new URL(request.url);
 
   const siteUrl = searchParams.get("url");
   console.log(siteUrl);
 
-  return NextResponse.json({
-    siteUrl
-  });
+  let res = new NextResponse("<head><img style='width: 100%; height: fit-content;' src='https://static.vecteezy.com/system/resources/previews/004/708/478/non_2x/purple-banner-design-modern-banner-template-design-with-purple-color-banner-for-social-media-cover-website-and-much-more-vector.jpg'/></head>")
+  .headers.set('Content-Type', 'text/html')
+
+
+  
+
+  return res
+
+  // return NextResponse.json({
+  //   siteUrl
+  // });
 }
