0c4967a83e0290ff97a916ad394e9bcf11c78e9a
DanielSanLopes <DanielSan_Lopes@outlook.com>
2025-12-27 00:59:15 -0300

    deploy issue fix... 4

--- app/api/banners/route.ts
+++ app/api/banners/route.ts
@@ -1,12 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
 
   const siteUrl = searchParams.get("url");
   console.log(siteUrl);
 
-  let res = new NextResponse("<head><img style='width: 100%; height: fit-content;' src='https://static.vecteezy.com/system/resources/previews/004/708/478/non_2x/purple-banner-design-modern-banner-template-design-with-purple-color-banner-for-social-media-cover-website-and-much-more-vector.jpg'/></head>")
+  let res = new Response("<head><img style='width: 100%; height: fit-content;' src='https://static.vecteezy.com/system/resources/previews/004/708/478/non_2x/purple-banner-design-modern-banner-template-design-with-purple-color-banner-for-social-media-cover-website-and-much-more-vector.jpg'/></head>")
   
   res.headers.set('Content-Type', 'text/html')
 
